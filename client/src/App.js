fd1bdbc858c424f924b1996599daca6c8c6ebc32
ktrkumarbablu <ktrkumarbablu@gmail.com>
2019-11-12 21:55:08 +0530

    new page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import {React,Component} from 'react';
-import logo from './logo.svg';
+import React,{Fragment} from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
 
-class App extends Component{
-  
-
-
-}
+const App=()=>(
+  <Router>
+    <Fragment>
+      <Navbar>    
+      </Navbar>
+      <Route exact path='/' component={Landing}></Route>      
+    </Fragment>
+  </Router>
+)
 
 export default App;
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+   <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Developer Connector</h1>
+          <p className="lead">
+            Space where you can take help of other expert Developer with all language.
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">Sign Up</a>
+            <a href="login.html" className="btn btn-light">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    )
+}
+export default Landing
