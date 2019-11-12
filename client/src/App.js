ffcaa4868ba8a511ad5f0e4a009fffb26738675a
ktrkumarbablu <ktrkumarbablu@gmail.com>
2019-11-12 19:53:26 +0530

    changes file

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-         <h1>Add client application </h1>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
 }
 
 export default App;
